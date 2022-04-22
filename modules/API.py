9b3834d848f0deddc9fec9d8f8d0e71c5772235e
OREdwardsJr <97556216+OREdwardsJr@users.noreply.github.com>
2022-04-21 22:27:45 -0700

    Removed test

--- modules/API.py
+++ modules/API.py
@@ -5,6 +5,3 @@ url = "https://data.cdc.gov/resource/n8mc-b4w4.json"
 r = requests.get(url)
 content = r.json()
 print(len(content))
-
-
-test = "https://data.cdc.gov/api/views/n8mc-b4w4/rows.json?accessType=DOWNLOAD"
